39032102ee1f9d312def5c057e30f351e2ec1763
Petr <pfory@seznam.cz>
2019-09-26 12:57:37 +0200

    +

--- fotovoltaika/fotovoltaika.ino
+++ fotovoltaika/fotovoltaika.ino
@@ -17,6 +17,8 @@
   
 */
 
+#include "Configuration.h"
+
 #include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
 #include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager
 #include <Ticker.h>
@@ -43,22 +45,12 @@ time_t getNtpTime();
 #define TIMEY   3
 #endif
 
-#define         CURRENT_UNIT     "A"
-#define         VOLTAGE_UNIT     "V"
-#define         POWER_UNIT       "W"
-#define         MIN_UNIT         "m"
-#define         AH_UNIT          "Ah"
-#define         V2MV              1000.f  //prevod volt na milivolt Y(in milivolt) = X(in volt) * V2MV
-
 //voltage and current meassurement
 Adafruit_ADS1115 ads1(0x48);  //mereni proudu ADDR to GND
 Adafruit_ADS1115 ads2(0x49);  //mereni napeti ADDR to VCC
 Adafruit_ADS1115 ads3(0x4B);  //mereni napeti ADDR to SCL
 
 
-#define LCDADDRESS  0x27
-#define LCDCOLS     20
-#define LCDROWS     4
 #include <LiquidCrystal_I2C.h>
 LiquidCrystal_I2C lcd(LCDADDRESS,LCDCOLS,LCDROWS);  // set the LCD
 #define PRINT_SPACE  lcd.print(" ");
@@ -78,108 +70,20 @@ byte customChar[] = {
   B00000
 };
 
-#define POZREGIN_CURRENTX                   1
-#define POZREGIN_CURRENTY                   1
-#define POZREGOUT_CURRENTX                  8
-#define POZREGOUT_CURRENTY                  1
-#define POZREGIN_VOLTAGEX                   1
-#define POZREGIN_VOLTAGEY                   2
-#define POZREGOUT_VOLTAGEX                  1
-#define POZREGOUT_VOLTAGEY                  2
-#define POZREGIN_POWERX                     2
-#define POZREGIN_POWERXY                    0
-#define POZREGOUT_POWERX                    9
-#define POZREGOUT_POWERXY                   0
-#define RELAY_STATUSX                       17
-#define RELAY_STATUSY                       3
-#define KOEFX                               8
-#define KOEFY                               2
-#define TEMPERATURE_X                       4
-#define TEMPERATURE_Y                       3
-#define RUNMINTODAY_X                      14 
-#define RUNMINTODAY_Y                       2
-#define AHPANELTODAY_X                     14 
-#define AHPANELTODAY_Y                      1
-
-char                  mqtt_server[40]       = "192.168.1.56";
-uint16_t              mqtt_port             = 1883;
-
 //for LED status
 Ticker ticker;
-
-//SW name & version
-#define     VERSION                          "0.80"
-#define     SW_NAME                          "Fotovoltaika"
-
-#define SEND_DELAY                           10000  //prodleva mezi poslanim dat v ms
-#define SENDSTAT_DELAY                       60000  //poslani statistiky kazdou minutu
-#define READADC_DELAY                        2000   //cteni ADC
 
 float   relayONVoltage                       = 13.5;
 float   relayOFFVoltage                      = 11.f;
 
 
-#define RELAY_ON                             HIGH
-#define RELAY_OFF                            LOW
 byte relayStatus                             = RELAY_OFF;
 byte manualRelay                             = 2;
-#define CURRENT4ON                           3.f //3A
 
-
-#define MAX                                  32767
-#define MIN                                  -32767
-
-#define ota
-#ifdef ota
-#define HOSTNAMEOTA   "fotovoltaika"
-#include <ArduinoOTA.h>
-#endif
-
-#define AUTOCONNECTNAME   HOSTNAMEOTA
-#define AUTOCONNECTPWD    "password"
-
-#define verbose
-#ifdef verbose
-  #define DEBUG_PRINT(x)                     Serial.print (x)
-  #define DEBUG_PRINT2(x,y)                  Serial.print (x,y)
-  #define DEBUG_PRINTDEC(x)                  Serial.print (x, DEC)
-  #define DEBUG_PRINTLN(x)                   Serial.println (x)
-  #define DEBUG_PRINTF(x, y)                 Serial.printf (x, y)
-  #define PORTSPEED 115200             
-  #define DEBUG_WRITE(x)                     Serial.write (x)
-  #define DEBUG_PRINTHEX(x)                  Serial.print (x, HEX)
-  #define SERIAL_BEGIN                       Serial.begin(PORTSPEED)
-#else
-  #define DEBUG_PRINT(x)
-  #define DEBUG_PRINT2(x,y)
-  #define DEBUG_PRINTDEC(x)
-  #define DEBUG_PRINTLN(x)
-  #define DEBUG_PRINTF(x, y)
-  #define DEBUG_WRITE(x)
-#endif 
 
 //ADC_MODE(ADC_VCC);
 
 uint32_t heartBeat                          = 0;
-
-
-char      mqtt_username[40]         = "datel";
-char      mqtt_key[20]              = "hanka12";
-char      mqtt_base[60]             = "/home/SolarMereni";
-char      static_ip[16]             = "192.168.1.116";
-char      static_gw[16]             = "192.168.1.1";
-char      static_sn[16]             = "255.255.255.0";
-char      mqtt_topic_weather[25]    = "/home/Meteo/Temperature";
-
-#define VIN                         A0 // define the Arduino pin A0 as voltage input (V in)
-#define LED2PIN                     D3 //stav rele
-#define LED1PIN                     D4 //
-//#define CHAROUTPIN                  D7 //vystup z regulatoru, na testovacim je D7 asi spaleny
-#define RELAY1PIN                   D8 //pin rele 1
-#define RELAY2PIN                   D6 //pin rele 2
-#define PIRPIN                      D5 //pin pir sensoru
-//SDA                               D2
-//SCL                               D1
 
 //mereni napeti   
 float      voltageRegInMin          = MAX; //vystup z panelu, rozsah 0-20V
@@ -204,15 +108,6 @@ float     AhPanelToday              = 0;
 float     AhOutToday                = 0;
 
 
-#define         CHANNEL_REG_IN_CURRENT          2
-#define         CHANNEL_REG_OUT_CURRENT         1
-#define         CHANNEL_VOLTAGE_SUPPLY          3
-
-#define         MVOLTDILEKADC1                  0.1875
-#define         MVOLTDILEKADC2                  0.1875
-
-#define         MVAMPERIN                       40.f        // 40mV = 1A
-#define         MVAMPEROUT                      100.f       // 100mV = 1A
 
 Adafruit_INA219 ina219_2; //intput
 Adafruit_INA219 ina219_1(0x41); //output
